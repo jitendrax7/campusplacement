99cc0c67ea5d52c045cefb95f92a1a70e909d249
jitendrax7 <rathourjitendrasingh472@gmail.com>
2025-11-22 23:47:10 +0530

    s

---
+++ server/middleware/upload.js
@@ -0,0 +1,19 @@
+import multer from "multer";
+const storage = multer.diskStorage({});
+
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "application/pdf") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only .pdf files are allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }, 
+});
+
+export default upload;
